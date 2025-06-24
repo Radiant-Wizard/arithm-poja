f4a747422c2e4836e876588b697d64b4ed6b5e0d
radiant_wizard <hei.taratra@gmail.com>
2025-06-24 08:49:30 +0300

    test: tested the fix of the variable new type of the sum() method

--- src/test/java/com/rw/arithm/endpoint/rest/controller/SumControllerTest.java
+++ src/test/java/com/rw/arithm/endpoint/rest/controller/SumControllerTest.java
@@ -8,13 +8,8 @@ class SumControllerTest {
   private final SumController subject = new SumController();
 
   @Test
-  void sum_test() {
+  void sumTest() {
     assertEquals(5, subject.sum(2, 3));
-  }
-
-  @Test
-  void sum_test_big_number() {
-    assertEquals(5_000_000_000L, subject.sum(2_000_000_000L, 3_000_000_000L));
   }
 
 }
