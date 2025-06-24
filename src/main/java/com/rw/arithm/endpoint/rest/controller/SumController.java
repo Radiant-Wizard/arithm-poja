0cec46a4ad608d0263b0397e27e21c35f41a14d8
radiant_wizard <hei.taratra@gmail.com>
2025-06-24 08:37:20 +0300

    chore: fixed indentation

--- src/main/java/com/rw/arithm/endpoint/rest/controller/SumController.java
+++ src/main/java/com/rw/arithm/endpoint/rest/controller/SumController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SumController {
+
   @GetMapping("/sum")
-  public int sum(@RequestParam int a, @RequestParam int b){
+  public int sum(@RequestParam int a, @RequestParam int b) {
     return a + b;
   }
 }
--- src/test/java/com/rw/arithm/endpoint/rest/controller/SumControllerTest.java
+++ src/test/java/com/rw/arithm/endpoint/rest/controller/SumControllerTest.java
@@ -1,12 +1,14 @@
 package com.rw.arithm.endpoint.rest.controller;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SumControllerTest {
   private final SumController subject = new SumController();
+
   @Test
-  void sumTest(){
+  void sumTest() {
     assertEquals(5, subject.sum(2, 3));
   }
 
