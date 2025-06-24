0882d72400425bbe47b4eaf1573210c880990266
radiant_wizard <hei.taratra@gmail.com>
2025-06-24 08:35:43 +0300

    feat: added a sum endpoint

---
+++ src/main/java/com/rw/arithm/endpoint/rest/controller/SumController.java
@@ -0,0 +1,13 @@
+package com.rw.arithm.endpoint.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SumController {
+  @GetMapping("/sum")
+  public int sum(@RequestParam int a, @RequestParam int b){
+    return a + b;
+  }
+}
